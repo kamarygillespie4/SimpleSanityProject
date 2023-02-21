de7fd2c6622ad7ef431d60a9b76dcfc4f4733d3f
Kamary <gillespiekamary@gmail.com>
2023-02-20 18:39:28 -0700

    starting fresh

--- food-catalog-app/food-catalog-studio/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'uru18bn5',
-    dataset: 'production'
-  }
-})
--- food-catalog-app/food-catalog-studio/schemas/index.js
+++
@@ -1,4 +0,0 @@
-import foods from './foods'
-import category from './category'
-
-export const schemaTypes = [foods, category]
